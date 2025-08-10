e2f3b8d21c957017d8bbecc9d46b785701c2bbb9
DevMultban <devmultban@gmail.com>
2025-08-10 12:21:58 -0300

    Atualização do sistema com novas funcionalidades

---
+++ public/assets/dist/js/pages/agendamento/agendamento.js
@@ -0,0 +1,109 @@
+$(function () {
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    /* initialize the calendar
+     -----------------------------------------------------------------*/
+
+    var Calendar = FullCalendar.Calendar;
+
+    var calendarEl = document.getElementById('calendar');
+
+    // initialize the external events
+    // -----------------------------------------------------------------
+
+    var calendar = new Calendar(calendarEl, {
+        events: {
+            url: "/get-agendamento",
+            type: 'POST',
+            extraParams: {
+                custom_param1: 'something',
+                custom_param2: 'somethingelse'
+            },
+            error: function () {
+                alert('there was an error while fetching events!');
+            },
+            color: 'yellow',   // a non-ajax option
+            textColor: 'black' // a non-ajax option
+        },
+        locale: 'pt-br',
+        initialView: 'dayGridMonth',
+        timeZone: 'UTC',
+        slotMinTime: "08:00:00",
+        slotMaxTime: "20:00:00",
+        selectable: true,
+        selectMirror: false,
+        dayMaxEvents: true, // allow "more" link when too many events
+        navLinks: true, // can click day/week names to navigate views
+        editable: false,
+        droppable: false, // this allows things to be dropped onto the calendar !!!
+        displayEventTime: true,
+        displayEventEnd: true,
+        firstDay: 1, // Monday as the first day of the week
+        slotDuration: '00:15:00', // 30 minutes slots
+        //slotLabelInterval: '00:15', // 1 hour intervals for time labels
+        slotLabelFormat: { hour: '2-digit', minute: '2-digit', hour12: false }, // 24-hour format for time labels
+        timeFormat: 'h(:mm)t',
+        // eventContent: function(arg) {
+        //     console.log('eventContent: ', arg);
+        //     // Customize the content of the event
+        //     // You can return a string or an object with html property
+        //     // Here we are returning a div with the event description
+        //     // and a dot for the event color
+        //   return { html: '<div class="fc-daygrid-event-dot"></div><div class="fc-event-time">'+arg.event.startStr+'</div><div class="fc-event-title">TEste</div>' };
+        // },
+        eventDidMount: function (info) {
+            console.log('eventDidMount: ', info.event);
+            new bootstrap.Tooltip(info.el, {
+                title: info.event.extendedProps.description, // Or whatever content you want
+                placement: 'top', // Adjust placement as needed
+                trigger: 'hover', // Show on hover
+                container: 'body', // Attach to the body to prevent clipping
+                delay: { "show": 500, "hide": 100 } // Optional delay for showing/hiding
+            });
+        },
+        eventTimeFormat: { // like '14:30:00'
+            hour: '2-digit',
+            minute: '2-digit',
+            meridiem: false
+        },
+        eventMouseEnter: function (info) {
+            console.log('Event: ', info.event);
+
+            // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+            // alert('View: ' + info.view.type);
+
+            // change the border color just for fun
+            info.el.style.cursor = 'pointer';
+        },
+        eventClick: function (info) {
+            console.log('eventClick: ', info.event);
+            // Show a tooltip with the event description
+            // alert('Event: ' + info.event.title);
+            // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+            // alert('View: ' + info.view.type);
+            Pace.restart();
+            Pace.track(function () {
+                // You can use the event ID to fetch more details or perform actions
+                console.log('Event ID: ', info.event.id);
+                window.location.href = "/agendamento/" + info.event.id + "/alterar";
+            });
+
+            // change the border color just for fun
+            info.el.style.borderColor = 'red';
+        },
+        headerToolbar: {
+            left: 'prev,next,today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+        },
+        themeSystem: 'bootstrap',
+
+    });
+
+    calendar.render();
+})
--- resources/views/Multban/agendamento/index.blade.php
+++ resources/views/Multban/agendamento/index.blade.php
@@ -94,144 +94,27 @@
 <script src="{{ asset('assets/plugins/fullcalendar-6.1.18/dist/locales/pt-br.global.min.js') }}"></script>
 
 <script src="{{ asset('assets/dist/js/app.js') }}"></script>
-<script src="{{ asset('assets/dist/js/pages/cliente/gridcliente.js') }}"></script>
+<script src="{{ asset('assets/dist/js/pages/agendamento/agendamento.js') }}"></script>
 
 <script type="text/javascript">
     $(document).ready(function () {
-        $('body').on('click', '#btnAgendamento', function (e) {
-            e.preventDefault();
-            $('#modalAgenda').modal('show');
-        });
 
-            @if ($message = Session::get('success'))
-                $("#empresa_id").val({{ Session::get('idModeloInserido') }})
-                toastr.success("{{ $message }}", "Sucesso");
-            @endif
-            @if ($message = Session::get('error'))
-                toastr.error("{{ $message }}", "Erro");
-            @endif
-            @if (count($errors) > 0)
-                @foreach ($errors->all() as $error)
-                    toastr.error("{{ $error }}", "Erro");
-                @endforeach
-            @endif
+        @if ($message = Session::get('success'))
+            $("#empresa_id").val({{ Session::get('idModeloInserido') }})
+            toastr.success("{{ $message }}", "Sucesso");
+        @endif
+        @if ($message = Session::get('error'))
+            toastr.error("{{ $message }}", "Erro");
+        @endif
+        @if (count($errors) > 0)
+            @foreach ($errors->all() as $error)
+                toastr.error("{{ $error }}", "Erro");
+            @endforeach
+        @endif
     });
 </script>
 
 
-<script>
-    $(function () {
-
-        var SITEURL = "{{ url('/') }}";
-
-
-
-$.ajaxSetup({
-
-    headers: {
-
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-
-    }
-
-});
-
-    /* initialize the calendar
-     -----------------------------------------------------------------*/
-    //Date for the calendar events (dummy data)
-    var date = new Date()
-    var d    = date.getDate(),
-    m    = date.getMonth(),
-    y    = date.getFullYear()
-
-    var Calendar = FullCalendar.Calendar;
-
-    var calendarEl = document.getElementById('calendar');
-
-    // initialize the external events
-    // -----------------------------------------------------------------
-
-    var calendar = new Calendar(calendarEl, {
-        events: SITEURL + "/get-agendamento",
-        locale: 'pt-br',
-    initialView: 'dayGridMonth',
- timeZone: 'UTC',
-    slotMinTime: "08:00:00",
-    slotMaxTime: "20:00:00",
-    selectable: true,
-    selectMirror: false,
-    dayMaxEvents: true, // allow "more" link when too many events
-    navLinks: true, // can click day/week names to navigate views
-    editable: false,
-    droppable: false, // this allows things to be dropped onto the calendar !!!
-    displayEventTime: true,
-    displayEventEnd: true,
-    firstDay: 1, // Monday as the first day of the week
-    slotDuration: '00:15:00', // 30 minutes slots
-    //slotLabelInterval: '00:15', // 1 hour intervals for time labels
-    slotLabelFormat: { hour: '2-digit', minute: '2-digit', hour12: false }, // 24-hour format for time labels
-  timeFormat: 'h(:mm)t',
-    // eventContent: function(arg) {
-    //     console.log('eventContent: ', arg);
-    //     // Customize the content of the event
-    //     // You can return a string or an object with html property
-    //     // Here we are returning a div with the event description
-    //     // and a dot for the event color
-    //   return { html: '<div class="fc-daygrid-event-dot"></div><div class="fc-event-time">'+arg.event.startStr+'</div><div class="fc-event-title">TEste</div>' };
-    // },
-    eventDidMount: function(info) {
-        console.log('eventDidMount: ', info.event);
-        new bootstrap.Tooltip(info.el, {
-            title: info.event.extendedProps.description, // Or whatever content you want
-            placement: 'top', // Adjust placement as needed
-            trigger: 'hover', // Show on hover
-            container: 'body', // Attach to the body to prevent clipping
-            delay: { "show": 500, "hide": 100 } // Optional delay for showing/hiding
-        });
-    },
-    eventTimeFormat: { // like '14:30:00'
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    meridiem: false
-  }
-    eventMouseEnter: function(info) {
-        console.log('Event: ', info.event);
-
-        // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-        // alert('View: ' + info.view.type);
-
-        // change the border color just for fun
-        info.el.style.cursor = 'pointer';
-     },
-    eventClick: function(info) {
-        console.log('eventClick: ', info.event);
-        // Show a tooltip with the event description
-        // alert('Event: ' + info.event.title);
-        // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-        // alert('View: ' + info.view.type);
-        Pace.restart();
-        Pace.track(function() {
-            // You can use the event ID to fetch more details or perform actions
-            console.log('Event ID: ', info.event.id);
-            window.location.href = SITEURL + "/agendamento/" + info.event.id + "/alterar";
-        });
-
-        // change the border color just for fun
-        info.el.style.borderColor = 'red';
-  },
-    headerToolbar: {
-        left  : 'prev,next,today',
-        center: 'title',
-        right : 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-      },
-    themeSystem: 'bootstrap',
-
-    });
-
-    calendar.render();
-  })
-</script>
 
 @if ($message = Session::get('success'))
 <script>
